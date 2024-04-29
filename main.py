98b32aa90a9173417e7f98d1e7d4d5effafac424
adam <109483705+adam-han@users.noreply.github.com>
2024-04-29 00:37:07 -0400

    initial set up

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+course_links = ["https://classes.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=1245&subject=CS&cournum=234", "https://classes.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=1245&subject=CS&cournum=231"]
+
+
+
